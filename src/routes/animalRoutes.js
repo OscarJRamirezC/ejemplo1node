b7d94b48d4c592b24d8210fa54d5c5aae27b2a04
Default <fsfd@gmail.com>
2024-04-18 20:21:47 -0500

    Final de clase

--- src/routes/animalRoutes.js
+++ src/routes/animalRoutes.js
@@ -1,3 +1,4 @@
+const verifyToken = require('./validate_token');
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
@@ -9,7 +10,7 @@ router.post("/animals", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
-router.get("/animals", (req, res) => {
+router.get("/animals", verifyToken, (req, res) => {
     animalSchema.find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
